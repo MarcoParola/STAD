80f899cc370c869739dfbb0d7649a2b47b7586fa
marco <marcoparola96@gmail.com>
2020-02-06 18:09:17 +0100

    test 6 classifiers using cross-validation

--- py/classifier.py
+++ py/classifier.py
@@ -1,9 +1,18 @@
 import csv
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
-fileFeature = "fileFeatures.tsv"
-fileClasses = "fileClasses.tsv"
+from sklearn.model_selection import cross_val_score
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+fileFeature = "fileFeatures1.tsv"
+fileClasses = "fileClasses1.tsv"
 
 input = []
 output = []
@@ -23,13 +32,53 @@ with open(fileFeature, 'r', encoding='utf-8') as features, open(fileClasses, 'r'
         input.append(rowInt)
     
     
-    
-tree = DecisionTreeClassifier()
+
+
+
+# CROSS VALIDATION, cv specify the number of iterations
+
+# DECISION TREE
+DecTree = DecisionTreeClassifier()
+print(cross_val_score(DecTree, input, output, cv = 6))
+
+fig = DecTree.fit(input, output)
+tree.plot_tree(fig)
+plt.show()
+
+# BAYESS
+bayess = GaussianNB()
+print(cross_val_score(bayess, input, output, cv = 6))
+
+
+# SVC
+svc = SVC()
+print(cross_val_score(svc, input, output, cv = 6))
+
+
+# K-NN (set k)
+knn = KNeighborsClassifier(5)
+print(cross_val_score(knn, input, output, cv = 6))
+
+
+# ADABOOST
+ada = AdaBoostClassifier()
+print(cross_val_score(ada, input, output, cv = 6))
+
+
+# RANDOM FOREST
+randForest = RandomForestClassifier()
+print(cross_val_score(randForest, input, output, cv = 6))
+
+
+
 
 # TRAINING AND PREDICTION 
-model = tree.fit(input, output)
-        
-print(model.predict_proba([[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1] ]))
+model = DecTree.fit(input, output)
 
+print(model.n_classes_)
+
+
+        
+#print(model.predict_proba([[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1] ]))
 
 
